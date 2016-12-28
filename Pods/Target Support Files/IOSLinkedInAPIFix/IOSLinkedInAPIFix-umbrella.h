163eab6a16f3bfb4f0556ea46fa99ffeca5c9f91
musharraf <mjmalik@msn.com>
2016-12-28 15:44:11 +0500

    Initial commit

---
+++ Pods/Target Support Files/IOSLinkedInAPIFix/IOSLinkedInAPIFix-umbrella.h
@@ -0,0 +1,12 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "LIALinkedInApplication.h"
+#import "LIALinkedInAuthorizationViewController.h"
+#import "LIALinkedInHttpClient.h"
+#import "NSString+LIAEncode.h"
+
+FOUNDATION_EXPORT double IOSLinkedInAPIFixVersionNumber;
+FOUNDATION_EXPORT const unsigned char IOSLinkedInAPIFixVersionString[];
+
---
+++ Pods/Target Support Files/LinkedinSwift/LinkedinSwift-umbrella.h
@@ -0,0 +1,13 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "LinkedinSwiftConfiguration.h"
+#import "LinkedinSwiftHelper.h"
+#import "LSHeader.h"
+#import "LSLinkedinToken.h"
+#import "LSResponse.h"
+
+FOUNDATION_EXPORT double LinkedinSwiftVersionNumber;
+FOUNDATION_EXPORT const unsigned char LinkedinSwiftVersionString[];
+
